645ca837675d9f6424f278602d411835075c0237
MoRan-LanShan <moran_hd@163.com>
2020-06-30 19:01:54 +0800

    1.新建公共模块server

---
+++ server/src/main/java/com/hd/course/server/domain/Test.java
@@ -0,0 +1,12 @@
+package com.hd.course.server.domain;
+
+import lombok.Data;
+
+@Data
+public class Test {
+
+    private String id;
+
+    private String name;
+
+}
---
+++ server/src/main/java/com/hd/course/server/mapper/TestMapper.java
@@ -0,0 +1,10 @@
+package com.hd.course.server.mapper;
+
+import com.hd.course.server.domain.Test;
+
+import java.util.List;
+
+public interface TestMapper {
+
+    List<Test> list();
+}
