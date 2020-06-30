b9e11d86a10af5543f0d99d00056b4c508c34a6d
MoRan-LanShan <moran_hd@163.com>
2020-06-30 18:32:10 +0800

    1.使用database关联数据库 2.新增数据库版本

---
+++ doc/db/all.sql
@@ -0,0 +1,6 @@
+drop table if exists `test`;
+CREATE TABLE `test` (
+  `id` varchar(255) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
